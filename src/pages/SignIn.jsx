4c3f97af205c967203d90753bef7df87deae0f54
Thierry <thierry.pollastri@gmail.com>
2026-08-06 19:33:44 +0200

    Integrate real SignIn page content
    Remplacement du composant coquille vide par le vrai contenu de sign-in.html, en réutilisant Navbar et Footer déjà extraits. Corrections JSX (className, htmlFor, Link, chemins d'images) et remplacement du lien-bouton du HTML statique par un vrai <button type="submit">. Pas encore de logique de soumission (prévue en Étape 2).

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,10 +1,36 @@
+import Navbar from '../components/Navbar.jsx'
+import Footer from '../components/Footer.jsx'
+
 function SignIn() {
   return (
-    <div>
-      <h1>Page de connexion</h1>
-    </div>
+    <>
+      <Navbar />
+      <main className="main bg-dark">
+        <section className="sign-in-content">
+          <i className="fa fa-user-circle sign-in-icon"></i>
+          <h1>Sign In</h1>
+          <form>
+            <div className="input-wrapper">
+              <label htmlFor="username">Username</label>
+              <input type="text" id="username" />
+            </div>
+            <div className="input-wrapper">
+              <label htmlFor="password">Password</label>
+              <input type="password" id="password" />
+            </div>
+            <div className="input-remember">
+              <input type="checkbox" id="remember-me" />
+              <label htmlFor="remember-me">Remember me</label>
+            </div>
+            <button type="submit" className="sign-in-button">
+              Sign In
+            </button>
+          </form>
+        </section>
+      </main>
+      <Footer />
+    </>
   )
 }
-
 
 export default SignIn
