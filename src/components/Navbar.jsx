ed891245c07a1aab75f9798fb6641d1d0d996ba5
Thierry <thierry.pollastri@gmail.com>
2026-08-06 10:24:34 +0200

    Add connection state logic to Navbar
    Ajout de useSelector pour lire state.user.token et afficher conditionnellement "Sign In" ou "Tony / Sign Out" selon l'état de connexion. Vérifié : la Navbar se met à jour automatiquement au clic sur les boutons de test login/logout, sans rechargement de page.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,9 @@
+import { useSelector } from 'react-redux'
 import { Link } from 'react-router-dom'
 
 function Navbar() {
+  const token = useSelector((state) => state.user.token)
+
   return (
     <nav className="main-nav">
       <Link className="main-nav-logo" to="/">
@@ -12,9 +15,20 @@ function Navbar() {
         <h1 className="sr-only">Argent Bank</h1>
       </Link>
       <div>
-        <Link className="main-nav-item" to="/sign-in">
-          <i className="fa fa-user-circle"></i> Sign In
-        </Link>
+        {token ? (
+          <>
+            <Link className="main-nav-item" to="/profile">
+              <i className="fa fa-user-circle"></i> Tony
+            </Link>
+            <Link className="main-nav-item" to="/">
+              <i className="fa fa-sign-out"></i> Sign Out
+            </Link>
+          </>
+        ) : (
+          <Link className="main-nav-item" to="/sign-in">
+            <i className="fa fa-user-circle"></i> Sign In
+          </Link>
+        )}
       </div>
     </nav>
   )
